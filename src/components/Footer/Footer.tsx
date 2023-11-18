4bd2f9c3dcb4feb4245659735004e17086468918
Hai Le Trung <letrunghai.92nl@gmail.com>
2023-11-18 21:59:52 +0700

    Update public assets

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="absolute right-3 bottom-2 text-sm font-semibold text-slate-500">
+    <div className="absolute right-3 bottom-2 text-sm font-semibold text-slate-500 cursor-none">
       Made by @m4tr1x
     </div>
   );
